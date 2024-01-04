91f177bbf56ccf06a2ed8d959f3c8b82e3c2f068
enRook <alcordoshan@gmail.com>
2024-01-04 20:01:57 +0700

    final final commit

--- app/src/main/java/com/sictlc/travel/SearchDestinationActivity.java
+++ app/src/main/java/com/sictlc/travel/SearchDestinationActivity.java
@@ -1,12 +1,12 @@
 package com.sictlc.travel;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Button;
 
 public class SearchDestinationActivity extends AppCompatActivity {
 
@@ -18,6 +18,8 @@ public class SearchDestinationActivity extends AppCompatActivity {
         ImageView fbImageView = findViewById(R.id.fb);
         ImageView gmailImageView = findViewById(R.id.gmail);
         ImageView twitterImageView = findViewById(R.id.twitter);
+
+        Button searchButton = findViewById(R.id.search);
 
         // Set click listeners for the ImageViews
         fbImageView.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +40,14 @@ public class SearchDestinationActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openWebsite("https://twitter.com");
+            }
+        });
+
+        // Set click listener for the search button to navigate to LogoutActivity
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SearchDestinationActivity.this, LogoutActivity.class));
             }
         });
     }
